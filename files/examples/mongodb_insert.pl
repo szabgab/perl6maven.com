da606ff7c779c769b938db3732c8136ae7501628
Gabor Szabo <gabor@szabgab.com>
2017-07-08 17:27:51 +0300

    publish

--- files/examples/mongodb_insert.pl
+++ files/examples/mongodb_insert.pl
@@ -1,18 +1,18 @@
 use BSON::Document;
 use MongoDB::Client;
 
-my $name = @*ARGS.shift || die "Usage: $*PROGRAM-NAME NAME";
-
-my $client = MongoDB::Client.new(:uri('mongodb://'));
-my $database = $client.database('myProject');
-
-my $req = BSON::Document.new: (
-  insert => 'people',
-  documents => [
-    ( 
-      name => $name,
-    ),
-  ]
-);
-$database.run-command($req);
+sub MAIN($name) {
+    my $client = MongoDB::Client.new(:uri('mongodb://'));
+    my $database = $client.database('myProject');
+    
+    my $req = BSON::Document.new: (
+      insert => 'people',
+      documents => [
+        ( 
+          name => $name,
+        ),
+      ]
+    );
+    $database.run-command($req);
+}
 
--- files/examples/mongodb_insert_with_types.pl
+++ files/examples/mongodb_insert_with_types.pl
@@ -2,17 +2,18 @@ use BSON::Document;
 use MongoDB::Client;
 use MongoDB::Database;
 
-my $name = @*ARGS.shift || die "Usage: $*PROGRAM-NAME NAME";
+sub MAIN($name) {
+    my MongoDB::Client $client .= new(:uri('mongodb://'));
+    my MongoDB::Database $database = $client.database('myProject');
+    
+    my BSON::Document $req .= new: (
+      insert => 'people',
+      documents => [
+        BSON::Document.new((
+          name => $name,
+        )),
+      ]
+    );
+    my BSON::Document $doc = $database.run-command($req);
+}
 
-my MongoDB::Client $client .= new(:uri('mongodb://'));
-my MongoDB::Database $database = $client.database('myProject');
-
-my BSON::Document $req .= new: (
-  insert => 'people',
-  documents => [
-    BSON::Document.new((
-      name => $name,
-    )),
-  ]
-);
-my BSON::Document $doc = $database.run-command($req);
